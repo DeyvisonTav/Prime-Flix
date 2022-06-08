e3710fe63585cc80b5811643622c690101e116c1
Deyvison Tavares <deyvisontav@gmail.com>
2022-06-08 12:47:37 -0300

    correcao avaliação e botão filmes

--- src/pages/Filme/Filme.jsx
+++ src/pages/Filme/Filme.jsx
@@ -35,19 +35,34 @@ export function Filme() {
 
   return (
     <div className="max-w-4xl m-2  snap-snap-proximity snap-y mt-4 md:w-full w-96 bg-white p-12 rounded-xl shadow-2xl shadow-black border-8 border-white">
-       <h1 className='mb-4 text-center md:text-2xl text-lg font-bold block'>{filme.title}</h1>
-       <img
-                className="snap-center md:h-auto h-56 w-auto rounded-xl  "
-                src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
-                alt={filme.title}
-              />
-       <h3 className= 'md:px-3 px-1  md:mt-4 mt-1 md:mb-4 mb-2 text-start md:text-2xl text-lg font-bold block'>Sinpse</h3>       
-       <span className='md:text-lg text-sm font-sans px-2  '>{filme.overview}</span>
-       <strong className='md:px-3 px-1 md:text-lg text-base underline font-bold'>avaliação: {filme.vote_average} /10</strong>
-       <div className='mt-6 flex flex-center md:justify-center md:space-x-3 space-x-2 '>
-         <button className="snap-center flex items-center justify-center md:py-3 py-1 md:px-10 px-5 text-white rounded-xl md:text-lg text-sm  bg-black pointer hover:bg-zinc-800 transition-colors">Salvar</button>
-         <button className="snap-center flex items-center justify-center md:py-3 py-1 md:px-10 px-5 text-white rounded-xl text-sm bg-black pointer hover:bg-zinc-800 transition-colors"><a href="#">Trailer</a></button>
-       </div>
+      <h1 className="mb-4 text-center md:text-2xl text-lg font-bold block">
+        {filme.title}
+      </h1>
+      <img
+        className="snap-center md:h-auto h-56 w-auto rounded-xl  "
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3 className="md:px-3 px-1  md:mt-4 mt-1 md:mb-4 mb-2 text-start md:text-2xl text-lg font-bold block">
+        Sinpse
+      </h3>
+      <span className="md:text-lg text-sm font-sans px-2 ">
+        {filme.overview}
+      </span>
+      <div>
+        <strong className="md:px-3 px-1 md:text-lg text-base  underline font-bold">
+          avaliação: {filme.vote_average} /10
+        </strong>{' '}
+      </div>
+
+      <div className="mt-6 flex flex-center justify-center  md:space-x-3 space-x-2 ">
+        <button className="snap-center flex items-center justify-center md:py-3 py-1 md:px-10 px-5 text-white rounded-xl md:text-lg text-sm  bg-black pointer hover:bg-zinc-800 transition-colors">
+          Salvar
+        </button>
+        <button className="snap-center flex items-center justify-center md:py-3 py-1 md:px-10 px-5 text-white rounded-xl text-sm bg-black pointer hover:bg-zinc-800 transition-colors">
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   )
 }
